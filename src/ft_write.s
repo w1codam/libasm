eac1467f22b7a8fc45e462da8f1b16d762944b39
Justus De groot <jde-groo@f1r4s16.codam.nl>
2023-11-28 15:44:48 +0100

    added read and write to test and fixed an errno problem

--- src/ft_write.s
+++ src/ft_write.s
@@ -4,4 +4,8 @@ section .text
 ft_write:
 	mov		rax, 1
 	syscall
+	cmp		rax, 0
+	jge		return		; if the syscall 'failed', it would put the negative value of errno in rax
+	mov		rax, -1
+return:
 	ret
--- src/main.c
+++ src/main.c
@@ -40,14 +40,15 @@ void check_strdup(char *input_str) {
     free(obtained_res);
 }
 
-// void check_write(int fd1, int fd2, char *str, int len) {
-//     dprintf(1, "Checking ft_write with string: %s and length: %d\n", str, len);
-//     char buffer1[BUFFER_SIZE] = {0};
-//     char buffer2[BUFFER_SIZE] = {0};
-//     ssize_t expected_res = write(fd1, str, len);
-//     ssize_t obtained_res = ft_write(fd2, str, len);
-//     dprintf(1, "Expected write size: %zd, Obtained: %zd\n", expected_res, obtained_res);
-// }
+void check_write(int fd1, int fd2, char *str, int len) {
+    dprintf(1, "Checking ft_write with string: %s and length: %d\n", str, len);
+	dprintf(1, "\n>");
+    ssize_t expected_res = write(fd1, str, len);
+	dprintf(1, "\n>");
+    ssize_t obtained_res = ft_write(fd2, str, len);
+    dprintf(1, "\nExpected write size: %zd, Obtained: %zd, errno: %d\n", expected_res, obtained_res, errno);
+	errno = 0;
+}
 
 // void check_valid_read(int fd1, int read_size) {
 //     dprintf(1, "Checking ft_read with file descriptor: %d and read size: %d\n", fd1, read_size);
@@ -62,55 +63,40 @@ void check_strdup(char *input_str) {
 //     dprintf(1, "Expected read size: %zd, Obtained: %zd\n", expected_res, obtained_res);
 // }
 
-// void check_read(int fd1, int fd2, int read_size) {
-//     dprintf(1, "Checking ft_read with file descriptor: %d and read size: %d\n", fd1, read_size);
-//     char buffer1[BUFFER_SIZE] = {0};
-//     char buffer2[BUFFER_SIZE] = {0};
-//     ssize_t expected_res = read(fd1, buffer1, read_size);
-//     ssize_t obtained_res = ft_read(fd2, buffer2, read_size);
-//     dprintf(1, "Expected read size: %zd, Obtained: %zd\n", expected_res, obtained_res);
-// }
+void check_read(int fd1, int fd2, int read_size) {
+    dprintf(1, "Checking ft_read with file descriptor: %d and read size: %d\n", fd1, read_size);
+    char buffer1[BUFFER_SIZE] = {0};
+    char buffer2[BUFFER_SIZE] = {0};
+	dprintf(1, "std read > ");
+    ssize_t expected_res = read(fd1, buffer1, read_size);
+	buffer1[expected_res] = 0;
+	dprintf(1, "asm read > ");
+    ssize_t obtained_res = ft_read(fd2, buffer2, read_size);
+	buffer2[obtained_res] = 0;
+    dprintf(1, "Expected read size: %zd, Obtained: %zd, errno: %d\n", expected_res, obtained_res, errno);
+	errno = 0;
+}
 
 
 int main(void) {
     char lengthy_string[] = "This is a veeery lengthy string used for testing. It contains multiple sentences and is long enough to test the functions.";
-    // ssize_t result;
-    // result = 0;
-	// char char_read[21];
 
-    // dprintf(1, "--------------------------\n");
-    // dprintf(1, "---- Testing ft_write ----\n");
-    // dprintf(1, "--------------------------\n");
-    // dprintf(1, "\n");
-    // int file_descriptor1 = open("mock-write.txt", O_WRONLY | O_TRUNC | S_IRUSR | S_IWUSR | S_IROTH);
-    // int file_descriptor2 = open("mock-ftwrite.txt", O_WRONLY | O_TRUNC | S_IRUSR | S_IWUSR | S_IROTH);
-    // check_write(file_descriptor1, file_descriptor2, "Hello I am a humongus trout", strlen("Hello I am a humongus trout"));
-    // check_write(file_descriptor1, file_descriptor2, "", strlen(""));
-    // check_write(file_descriptor1, file_descriptor2, lengthy_string, strlen(lengthy_string));
-    // check_write(-1, -1, lengthy_string, strlen(lengthy_string));
-	// close(file_descriptor1);
-    // close(file_descriptor2);
+    dprintf(1, "--------------------------\n");
+    dprintf(1, "---- Testing ft_write ----\n");
+    dprintf(1, "--------------------------\n");
+    dprintf(1, "\n");
+    check_write(1, 1, "Hello I am a humongus trout", strlen("Hello I am a humongus trout"));
+    check_write(1, 1, "", strlen(""));
+    check_write(2, 2, lengthy_string, strlen(lengthy_string));
+    check_write(-1, -1, lengthy_string, strlen(lengthy_string));
 
-    // dprintf(1, "\n");
-    // dprintf(1, "-------------------------\n");
-    // dprintf(1, "---- Testing ft_read ----\n");
-    // dprintf(1, "-------------------------\n");
-    // dprintf(1, "\n");
-
-
-    // dprintf(1, "---- functional file descriptor ----\n");
-    // check_valid_read(file_descriptor1, 0);
-    // check_valid_read(file_descriptor1, 1);
-    // check_valid_read(file_descriptor1, 5);
-    // dprintf(1, "\n");
-    
-    // dprintf(1, "---- non-functional file descriptor ----\n");
-    // file_descriptor1 = open("", O_RDONLY);
-    // file_descriptor2 = open("", O_RDONLY);
-    // check_read(file_descriptor1, file_descriptor2, BUFFER_SIZE);
-    // check_read(file_descriptor1, file_descriptor2, 0);
-    // check_read(file_descriptor1, file_descriptor2, 1);
-    // check_read(file_descriptor1, file_descriptor2, 5);
+    dprintf(1, "\n");
+    dprintf(1, "-------------------------\n");
+    dprintf(1, "---- Testing ft_read ----\n");
+    dprintf(1, "-------------------------\n");
+    dprintf(1, "\n");
+	check_read(0, 0, BUFFER_SIZE - 1);
+	check_read(-1, -1, BUFFER_SIZE - 1);
 
     dprintf(1, "\n");
     dprintf(1, "---------------------------\n");
